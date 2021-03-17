e0f8b8ffd784bbfba478dff8963b4e5d303ee940
larisa mac <larisa@MacBook-Pro-larisa.local>
2021-03-17 15:42:04 +0200

    am bagat logo

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,13 +1,34 @@
 import { AppBar, Container, Toolbar, Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(1),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  logo: {
+    height: "30px",
+  },
+}));
 
 export default function Navbar() {
+  const classes = useStyles();
   return (
     <>
       <AppBar position="fixed">
         <Container fixed>
           <Toolbar>
             <a href="/">
-              <img src="/img/Logo1.svg" alt="Colors Logo" />
+              <img
+                src="/img/Logo1.svg"
+                alt="Colors Logo"
+                className={classes.logo}
+              />
             </a>
             <Box></Box>
           </Toolbar>
