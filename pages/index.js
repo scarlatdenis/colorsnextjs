7483bb3662f162aa13d10e15238d67a094632524
larisa mac <larisa@MacBook-Pro-larisa.local>
2021-03-17 12:12:23 +0200

    started

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 import Head from "next/head";
+import { AppBar, Container, Toolbar } from "@material-ui/core";
 import styles from "../styles/Home.module.css";
 
 export default function HomePage() {
-  return <div></div>;
+  return (
+    <div>
+      <AppBar position="fixed">
+        <Container fixed>
+          <Toolbar></Toolbar>
+        </Container>
+      </AppBar>
+    </div>
+  );
 }
